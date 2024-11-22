bb72eff521abc42d0c9d538b2d591dc9ac4e46d7
Tymek <tymoteuszsosin@gmail.com>
2024-11-22 19:01:20 +0100

    Przycisk
    Przycisk graj

---
+++ Assets/Controller.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button Button; // Drag your button here in the inspector
+
+    void Start()
+    {
+        // Ensure the button is assigned
+        if (Button != null)
+        {
+            // Add a listener to the button to call LoadScene when clicked
+            Button.onClick.AddListener(LoadScene);
+        }
+        else
+        {
+            Debug.LogError("Button not assigned!");
+        }
+    }
+
+    void LoadScene()
+    {
+        // Load the scene by name
+        SceneManager.LoadScene("Poziom1"); // Replace with the name of your target scene
+    }
+}
